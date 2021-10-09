a409bad5a19a9e0ea600e1676e9d91a5ca4ccde2
esteban aragon <aragone@3il.fr>
2021-10-09 20:43:42 +0200

    feat: Writer now checks for Tag completeness
    If a tag is not complete, ie:
    - missing an id
    - missing its shoe type
    - missing a main color
    
    the writer now does nothing, in frustration.

--- src/interface/Writer.py
+++ src/interface/Writer.py
@@ -18,13 +18,15 @@ class Writer(json.JSONEncoder):
     def outputTagAsJson(tag:Tag, outputFilePath:str='../out/data.json'):
         '''Writes the given tag under the Json format in the output file: ../out/data.json .
         
-        The json string of the given tag is appended to the file in a new line.
+        Checks if the tag is complete first. 
+        If it is, the json string of the given tag is appended to the file in a new line.
         
         This function may seem simple, but it was very painful to make. I had lots of research to do before getting smthg that actually works.
         
         With love, 
         
         Esteban'''
+        if not tag.isComplete(): return
         data = jsonpickle.encode(tag)
         file_object = open(outputFilePath, 'a')
         file_object.write("\n"+data)
