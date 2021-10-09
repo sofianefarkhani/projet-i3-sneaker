6ce737b159ae495ef8e7c8e11efdc117102437d7
esteban aragon <aragone@3il.fr>
2021-10-09 17:58:36 +0200

    doc: Documented the Writer class.
    Merged the Writer branch into the Interfaces-Tags-and-colors.
    Documented the Writer class with an explanation of what the writer does.

--- src/interface/Writer.py
+++ src/interface/Writer.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 import json
+from Tag import Tag
+
+
 class Writer(json.JSONEncoder):
+    '''The Writer writes JSON. 
+    
+    The Writer takes a Tag and image upon request from the BlackBox. It writes the tag into the appropriate json file.'''
 
     def json_convert(self, obj): 
         if isinstance(obj, Tags):
@@ -9,3 +15,4 @@ class Writer(json.JSONEncoder):
         raise ValueError("Writer take only Tags object, your object is not Tags")
         return json.JSONEncoder.json_convert(self, obj)
         
+
