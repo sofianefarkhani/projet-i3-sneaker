1bd86acf22430281c768d8a8df4367b61eb3f50c
Mathilde Nicolas <mathilde.nicolas54170@gmail.com>
2021-10-14 16:44:19 +0200

    feat : option 1 menu
    forward of the option 1

--- src/menu.py
+++ src/menu.py
@@ -3,83 +3,174 @@
 import jsonpickle
 from Data.Tag import Tag
 from menuPkg.MenuImageLoader import MenuImageLoader
+from Data.BasicColors import ColorEnum
+from enum import Enum
 
 
 continued = True
 while(continued):
 
-    print("\nOption 1 - Taper 1") 
+    print("\nAdd some datas - Press 1") 
+    print("Modify some datas - Press 2")
+    print("Get some informations about an image - Press 3")
+    print("Exit the menu - Press 4")
 
-    """
-    Créer du data - il donne une image et on doit mettre les données (remplir à la main les données d'une image
-    quand il demande la couleur et le type -> puisse mettre ? -> rappel des données dispo
-    confirmation des données à la fin (demande)
-    dans un fichier json -> vérifie que l'image n'est pas déjà enregistré si enregistré demande quelles données gardées
-    """
-
-    print("Option 2 - Taper 2") #Modifier les data d'une image
-    print("Avoir les informations d'une image - Taper 3")
-    print("Quitter le menu - Taper 4")
-
-    choice = int(input("Que voulez-vous faire ? \n"))
+    choice = int(input("What do you want to do ? \n"))
         
     if(choice == 1):
-        print("vous avez choisi l'option 1")
+        #Get the new ID
+        file_object = open("../out/testData.json", 'r')
+        lines = file_object.readlines()
+        size = len(lines)
+        lastLine = jsonpickle.decode(lines[-1])
+        indexLastLine = lastLine.databaseID
+
+        #Get the type
+        boucleType = True
+        while(boucleType):
+            print("If your shoes are low - Press 0")
+            print("If your shoes are high - Press 1")
+            print("If you don't know - Press 2")
+            choiceType = int(input("Enter the type of your shoes : "))
+            if(choiceType == 0 or choiceType == 1):
+                boucleType = False
+            elif(choiceType == 2):
+                choiceType = None
+                boucleType = False
+            else:
+                print("This option doesn't exist... Choose something else...\n")
+
+        #Récup la couleur principal -> ? donne les couleurs existantes -> si nom pas dans base proposée de changer de couleur, et de créer le rgb
+        boucleColorPrincipal = True
+        while(boucleColorPrincipal):
+            print("What is the principal color in your shoes ?")
+            print("If you don't want to write it, write none")
+            choiceColorPrincipal = str(input("Enter your color : "))
+            if choiceColorPrincipal == "":
+                print("You didn't enter any color...")
+                continue
+            else:
+                choiceColorPrincipal.upper
+                if choiceColorPrincipal == "NONE" : 
+                    colorsFirst = None
+                    boucleColorPrincipal = False
+                elif choiceColorPrincipal not in ColorEnum:
+                    print("Your color isn't in our database")
+                    print("If you change to change the color - Press 1")
+                    print("If you want to add the color - Press 2")
+                    choiceAddDatabase = int(input("Enter your choice : "))
+                    if choiceAddDatabase == 1 :
+                        continue
+                    elif choiceAddDatabase == 2:
+                        colorR = int(input("Enter your value between 0 and 255 for Red : "))
+                        colorG = int(input("Enter your value between 0 and 255 for Green : "))
+                        colorB = int(input("Enter your value between 0 and 255 for Blue : "))
+
+                        if colorR > 255 : colorR=255
+                        elif colorR < 0 : colorR=0
+                        if colorG > 255 : colorG=255
+                        elif colorG < 0 : colorG=0
+                        if colorB > 255 : colorB=255
+                        elif colorB < 0 : colorB=0
+
+                        colorsFirst = [colorR, colorG, colorB]
+                        boucleColorPrincipal = False
+                    else:
+                        print("This choice doesn't exist...")
+                        continue
+
+        #Récup la couleur secondaire
+        boucleColorSecondaire = True
+        while(boucleColorSecondaire):
+            print("What is the secondary color in your shoes ?")
+            print("If you don't want to write it, write none")
+            choiceColorsecond = str(input("Enter your color : "))
+            if choiceColorsecond == "":
+                print("You didn't enter any color...")
+                continue
+            else:
+                choiceColorsecond.upper
+                if choiceColorsecond == "NONE" : 
+                    colorsSecond = None
+                    boucleColorSecondaire = False
+                elif choiceColorsecond not in ColorEnum:
+                    print("Your color isn't in our database")
+                    print("If you change to change the color - Press 1")
+                    print("If you want to add the color - Press 2")
+                    choiceAddDatabase = int(input("Enter your choice : "))
+                    if choiceAddDatabase == 1 :
+                        continue
+                    elif choiceAddDatabase == 2:
+                        colorR = int(input("Enter your value between 0 and 255 for Red : "))
+                        colorG = int(input("Enter your value between 0 and 255 for Green : "))
+                        colorB = int(input("Enter your value between 0 and 255 for Blue : "))
+
+                        if colorR > 255 : colorR=255
+                        elif colorR < 0 : colorR=0
+                        if colorG > 255 : colorG=255
+                        elif colorG < 0 : colorG=0
+                        if colorB > 255 : colorB=255
+                        elif colorB < 0 : colorB=0
+
+                        colorsSecond = [colorR, colorG, colorB]
+                        boucleColorSecondaire = False
+                    else:
+                        print("This choice doesn't exist...")
+                        continue
+
     elif(choice == 2):
-        print("vous avez choisi l'option 2")
+        print("You chose option 2")
 
     elif(choice == 3):
-        #Pour le moment il n'y a pas de nom
-
         boucleId = True
         boucleName = False
         hasId = False
         hasName = False
 
         while(boucleId):
-            print("Si vous avez l'id de l'image - Taper 1")
-            print("Sinon taper 2")
-            choixId = int(input("choix : "))
+            print("If you have the ID of the image - Press 1")
+            print("Else press 2")
+            choixId = int(input("choice : "))
             if(choixId == 1):
-                print("Vous connaissez l'id de l'image")
-                idImage = int(input("Entrez l'id de l'image : "))
+                print("Vous know the ID of the image")
+                idImage = int(input("Enter the ID : "))
                 hasId = True
                 boucleId = False
             elif(choixId == 2):
-                print("Vous ne connaissez pas l'id de l'image\n")
+                print("You don't know the ID\n")
                 boucleName = True
                 boucleId = False
             else:
-                print("Ce choix n'existe pas\n")
+                print("This choice doesn't exist... Choose something else\n")
 
         while(boucleName):
-            print("Si vous avez le nom de l'image - Taper 1")
-            print("Sinon taper 2")
-            choixName = int(input("choix : "))
+            print("If you have the name of the image - Press 1")
+            print("Else press 2")
+            choixName = int(input("choice : "))
             if(choixName == 1):
-                print("Vous connaissez le nom de l'image")
-                nameImage = str(input("Entrez le nom de l'image : "))
+                print("You know the name of the image")
+                nameImage = str(input("Enter the name : "))
                 hasName = True
                 boucleName = False
             elif(choixName == 2):
-                print("Vous ne connaissez pas le nom de l'image\n")
+                print("You don't know the name\n")
                 boucleName = False
             else:
-                print("Ce choix n'existe pas\n")
+                print("This choice doesn't exist... Choose something else\n")
         
         if(hasId):
             theTag = MenuImageLoader.loadImageById(idImage)
             print(theTag.toString())
             continue    
         elif(hasName):
-            print("on a le nom\n")
+            print("We have the name\n")
             continue
         else:
-            print("Vous n'avez aucunes informations pour avoir accès aux données...\n")
+            print("You don't have any information to access some datas...\n")
             continue
 
     elif(choice == 4):
         continued = False
     else:
-        print("Votre choix n'existe pas, veuillez rechoisir votre choix")
+        print("Your choice doesn't exist... Choose something else...\n")
     
