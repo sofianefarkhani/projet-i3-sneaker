b10a806989cabf364db0425247872777db69adbb
esteban aragon <aragone@3il.fr>
2021-10-26 17:44:03 +0200

    feat: ConfigLoader a bit less chock-a-block
    ConfigLoader will now remember the last loaded vars, so that we do not need to go search them in the file each time.

--- src/interface/ConfigLoader.py
+++ src/interface/ConfigLoader.py
@@ -2,16 +2,31 @@ import yaml
 
 class ConfigLoader:
     configFile = '../config/config.yaml'
+    
+    lastLoadedVars = dict()
      
     def getVariable(*varPath):
+        varPathAsString = ""
+        for var in varPath: varPathAsString+="/"+var
+        
+        print (varPath)
+        
+        if varPathAsString in ConfigLoader.lastLoadedVars:
+            return ConfigLoader.lastLoadedVars[varPathAsString]
+        else: 
+            return ConfigLoader.getVarFromFile(varPath)
+    
+    def getVarFromFile(varPath):
+        '''Gets the asked for variable from the config file.'''        
+        
         with open(ConfigLoader.configFile) as f:
             configData = yaml.load(f, Loader=yaml.FullLoader)
-            
-            validPath = 'In the file: '+ConfigLoader.configFile
-            
             for i in range(len(varPath)):
-                if dTemp := configData[varPath[i]]:
+                if varPath[i] in configData:
                     configData = configData[varPath[i]]
                 else:
-                    return None
+                    raise ValueError('No such value in the config file: '+str(varPath)+"\n(failed at: "+str(varPath[i]))
             return configData
+        
+    def refreshValues():
+        ConfigLoader.lastLoadedVars = dict()
