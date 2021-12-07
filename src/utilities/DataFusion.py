988b0e2891ddcb58fd2c21aeee1c2cdf3deed69e
Mathilde Nicolas <63719999+Morqua54@users.noreply.github.com>
2021-12-07 09:54:58 +0100

    feat : Datafusion upgrade

--- src/utilities/DataFusion.py
+++ src/utilities/DataFusion.py
@@ -1,12 +1,14 @@
 import os
 from utilities.configUtilities.BBConfig import BBConfig
-import json 
+import json
+import shutil
 
 class DataFusion :
 
     def fusionJson():
         jsonObjectList = []
         pathFile = BBConfig.getTempOutput()
+        outPutFile = ("../out/data.json")
         for fileName in os.listdir(pathFile):
             DataFusion.getObjectFromFile(pathFile+"/"+fileName,jsonObjectList)
         DataFusion.getSortedListe(jsonObjectList)
@@ -15,11 +17,19 @@ class DataFusion :
         while(len(jsonObjectList)>0):
             tempObject = jsonObjectList.pop(0)
             if(tempObject["IDProduct"] != tempProduct):
-                DataFusion.fusionProduct(tempList.copy())
+                dataToAdd = DataFusion.fusionProduct(tempList.copy())
                 tempList = []
                 tempProduct = tempObject["IDProduct"]
+                if(dataToAdd != None):
+                    fileData = open(outPutFile, "a")
+                    newLineToWrite = json.dumps(dataToAdd) + "\n"
+                    fileData.write(newLineToWrite)
+                    fileData.close()
             else:
                 tempList.append(tempObject)
+
+        shutil.rmtree(pathFile,ignore_errors=True)
+        os.mkdir("../out/temp")
 
     def decodeJson(string:str):
         return json.loads(string)
@@ -42,6 +52,9 @@ class DataFusion :
             sumProbProb = 0
             dictColorsMain = {}
             dictColorsSecondary = {}
+            finalList = []
+            finalDict = {}
+            interDict = {}
             for dict in list:
                 #concaténer les img
                 listImg.append(dict["img"])
@@ -50,16 +63,38 @@ class DataFusion :
                 sumProbProb = sumProbProb + float(dict["shoeProb"])
 
                 #COLORWAY MOYENNE -> à la majorité -> dico avec détection couleur et nombre
-
-                if(len(dictColorsMain) == 0):
-                    dictColorsMain[dict["Colorway"]["mainColor"]["color"]] = 1
-                else:
-                    if(dict["Colorway"]["mainColor"]["color"] in dictColorsMain):
-                        dictColorsMain[dict["Colorway"]["mainColor"]["color"]] += 1
-                    else:
+                if("Colorway" in dict):
+                    if(len(dictColorsMain) == 0):
                         dictColorsMain[dict["Colorway"]["mainColor"]["color"]] = 1
+                    else:
+                        if(dict["Colorway"]["mainColor"]["color"] in dictColorsMain):
+                            dictColorsMain[dict["Colorway"]["mainColor"]["color"]] += 1
+                        else:
+                            dictColorsMain[dict["Colorway"]["mainColor"]["color"]] = 1
+
+                    if(len(dict["Colorway"]) > 1):
+                        if(len(dictColorsSecondary) == 0):
+                            dictColorsSecondary[dict["Colorway"]["secondaryColor"]["color"]] = 1
+                        else:
+                            if(dict["Colorway"]["secondaryColor"]["color"] in dictColorsSecondary):
+                                dictColorsSecondary[dict["Colorway"]["secondaryColor"]["color"]] += 1
+                            else:
+                                dictColorsSecondary[dict["Colorway"]["secondaryColor"]["color"]] = 1
 
             shoeProb = sumProbProb/len(list)
             mainColor = max(dictColorsMain, key=dictColorsMain.get)
+            if(len(dictColorsSecondary) != 0):
+                secondaryColor = max(dictColorsSecondary, key=dictColorsSecondary.get)
+            else:
+                secondaryColor = None
 
-            print("IDProduct : ",IDProduct," style : ",style," Liste Images : ",listImg," proba shoe : ", shoeProb, " couleur principal : ",mainColor)
+            interDict["lstImg"]=listImg
+            interDict["style"]=style
+            interDict["mainColor"]=mainColor
+            if(secondaryColor != None):
+                if(secondaryColor != mainColor):
+                    interDict["secondaryColor"]=secondaryColor
+            interDict["probaShoes"]=shoeProb
+            
+            finalDict[IDProduct] = interDict
+            return finalDict
