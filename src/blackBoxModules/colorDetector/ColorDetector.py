66f59c693891f3ce830668ffaa6c664efd46480e
Mathilde Nicolas <63719999+Morqua54@users.noreply.github.com>
2021-12-07 09:56:07 +0100

    feat : ColorDetection
    check if there is 2 keys and not just 1

--- src/blackBoxModules/colorDetector/ColorDetector.py
+++ src/blackBoxModules/colorDetector/ColorDetector.py
@@ -245,10 +245,11 @@ class ColorDetector:
                         listKeySuppr.append(key)
             else:
                 keys = dict.keys()
-                premierElem = str([*keys][0])
-                deuxiemeElem = str([*keys][1])
-                if (dict[premierElem] != dict[deuxiemeElem]):
-                    dict.pop(min(dict, key=dict.get))
+                if(len(keys) > 1) :
+                    premierElem = str([*keys][0])
+                    deuxiemeElem = str([*keys][1])
+                    if (dict[premierElem] != dict[deuxiemeElem]):
+                        dict.pop(min(dict, key=dict.get))
 
             if(len(listKeySuppr) != 0):
                 dict.pop(key)
