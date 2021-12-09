3ec8c5e8edc51789a38658681ac3588447c05d6e
Vivien Maugarnis <>
2021-12-09 11:28:32 +0100

    Feat: few changes in utilsAI

--- src/utilsAI.py
+++ src/utilsAI.py
@@ -1,7 +1,5 @@
 from os import path
 
-from predictAI.getPredictionAI import getPredictionShoesOrNot
-
 
 def getStringToAddToTrainData(nameOfFile, isAShoes, needBreakLine=True):
     return '{"py/object": "Data.TrainDataElement.TrainDataElement", "imageName": "'+nameOfFile+'", "isThereAShoe": true, "shoeType": {"py/reduce": [{"py/type": "Data.Type.Type"}, {"py/tuple": ['+str(isAShoes)+']}]}}' + ("\n" if needBreakLine else "")
@@ -58,33 +56,49 @@ def showImage(imagePath):
     import cv2
     im = cv2.imread(imagePath)
     im=cv2.resize(im, (200, 200))
-    cv2.imshow("test", im)
+    name = imagePath.split("/")[-1].split("\\")[-1]
+    cv2.imshow( name, im)
     cv2.waitKey()
+    cv2.destroyAllWindows()
 
-def getRandomImageNotInTrainData(numberOfImage, pathTrainData, pathAllImage = None, pathFileAllImage = None):
+def getRandomImageNotInTrainData(numberOfImage, pathTrainData, pathAllImage, pathFileAllImage, lstHashImages):
     import random
     import hashlib
 
     lstAllImage = []
     if pathAllImage is not None:
+        print("load all name img")
         f = open(pathFileAllImage, "r")
         for line in f:
             lstAllImage.append(line.replace("\n", ""))
+        print("all name img loaded")
     else:
         lstAllImage.extend(listerFilesInFolder(pathAllImage))
+        print("list all file from folder")
     (lst_name_shoes, lst_name_other) = getNameImageFromTrainData(pathTrainData)
     lstAllImageUseInTrainData = []
     lstAllImageUseInTrainData.extend(lst_name_shoes)
     lstAllImageUseInTrainData.extend(lst_name_other)
 
+    
     lstChoice = []
     lstHashOfChoice = []
-    for img in lstAllImageUseInTrainData:
-        with open(pathAllImage+img, "rb") as f:
-            bytes = f.read()
-            hash = hashlib.sha256(bytes).hexdigest()
-            lstHashOfChoice.append(hash)
+    # for img in lstAllImageUseInTrainData:
+    #     with open(pathAllImage+img, "rb") as f:
+    #         bytes = f.read()
+    #         hash = hashlib.sha256(bytes).hexdigest()
+    #         lstHashOfChoice.append(hash)
+    #         if(len(lstHashOfChoice)%100 == 0):
+    #             print("get hash:" + str(len(lstHashOfChoice)) + "/" + str(len(lstAllImageUseInTrainData)))
+
+    for imgName in lstAllImageUseInTrainData:
+        if imgName in lstHashImages:
+            lstHashOfChoice.append(lstHashImages[imgName])
+        else:
+            print("ERROR, NO HASH FOR : " + imgName)
+
     for i in range(numberOfImage):
+        print("select choice: " + str(i) +"/"+ str(numberOfImage))
         isOk = False
         while not isOk:
             choice = random.choice(lstAllImage)
@@ -121,7 +135,10 @@ def getPrediction(imagePath):
     img = loadImage(imagePath)
     return getPredictionShoesOrNot(img, model)[0][0]
 
-
+def getListHash(filePath):
+    import json
+    with open(filePath) as json_file:
+        return json.load(json_file)
 
 
 # (lst_name_shoes, lst_name_other) = getNameImageFromTrainData()
@@ -145,13 +162,55 @@ def getPrediction(imagePath):
 # pathSource = "/mnt/424cf323-70f0-406a-ae71-29e3da370aec/Sneaker-data/test_temp/Nouveau dossier/test/test1/"
 # showImage(pathSource + listerFilesInFolder(pathSource)[0])
 
-lstImage = getRandomImageNotInTrainData(30, "../in/trainDataV3.json", "D:/Images/resources/", "C:/Users/moonc/Desktop/lst_nom.txt")
-path = "D:/Images/resources/"
-for x in lstImage:
-    print(x + " -> ", end="")
-    predict = getPrediction(path + x)
-    print(("shoe" if predict> 0.5 else "other"), end="")
-    proba = predict-0.5 if predict > 0.5 else 0.5-predict
-    proba*=2
-    print("  proba=" + str(proba))
-    showImage(path + x)
+# lstImage = getRandomImageNotInTrainData(30, "../in/trainDataV3.json", "/home/vedoc/Documents/sftpDrive/", "/home/vedoc/Bureau/lst_nom.txt")
+# path = "/home/vedoc/Documents/sftpDrive/"
+# for x in lstImage:
+#     print(x + " -> ", end="")
+#     predict = getPrediction(path + x)
+#     print(("shoe" if predict> 0.5 else "other"), end="")
+#     proba = predict-0.5 if predict > 0.5 else 0.5-predict
+#     proba*=2
+#     print("  proba=" + str(proba))
+#     showImage(path + x)
+
+
+
+
+
+# pathImage = "/home/vedoc/Documents/sftpDrive/"
+# lst_all_img = getNameImageFromTrainData("../in/trainDataV3.json")
+# lstFileInFolder = listerFilesInFolder(pathImage)
+
+# lstHash = getListHash("/home/vedoc/Bureau/lstHashImg.txt")
+
+# lstImage = getRandomImageNotInTrainData(100, "../in/trainDataV3.json", pathImage, "/home/vedoc/Bureau/lst_nom.txt", lstHash)
+# for x in lstImage:
+#     print(x + " -> ", end="")
+#     predict = getPrediction(pathImage + x)
+#     print(("shoe" if predict> 0.5 else "other"), end="")
+#     proba = predict-0.5 if predict > 0.5 else 0.5-predict
+#     proba*=2
+#     print("  proba=" + str(proba))
+#     showImage(pathImage + x)
+
+
+
+
+lstHash = getListHash("/home/vedoc/Bureau/lstHashImg.txt")
+
+
+
+lstH = []
+lstName = []
+a = 0
+for i in lstHash:
+    if lstHash[i] not in lstH:
+        lstH.append(lstHash[i])
+        lstName.append(i)
+        a = a+1
+        if a == 10:
+            break
+
+import json
+with open('data.txt', 'w') as outfile:
+    json.dump(lstName, outfile)
