65b867e2ab7827a8f29517df3ba0fc19fb080ff5
Mathilde Nicolas <63719999+Morqua54@users.noreply.github.com>
2022-01-03 15:21:00 +0100

    feat : correction bug white shoes

--- src/blackBoxModules/preprocess/BackgroundSuppression.py
+++ src/blackBoxModules/preprocess/BackgroundSuppression.py
@@ -4,9 +4,11 @@
 import cv2
 import numpy as np
 import ast
+from Data.Color import Color
 
 from utilities.configUtilities.BBConfig import BBConfig
 from blackBoxModules.preprocess.ContrastAndBrightness import ContrastAndBrightness
+from utilities.configUtilities.ConfigLoader import ConfigLoader
 
 class BackgroundSuppression:
 
@@ -24,21 +26,39 @@ class BackgroundSuppression:
         __MASK_COLOR = BBConfig.getBackground()
         imagesNoBg = []
         if image is not None:
+            
             contrast = ContrastAndBrightness.getContrastValue(image)
             gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-            
-            if contrast >= 0.99:
+
+            average = image.mean(axis=0).mean(axis=0)
+            pixels = np.float32(image.reshape(-1, 3))
+            n_colors = 1
+            criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 0.1)
+            flags = cv2.KMEANS_RANDOM_CENTERS
+            _, labels, palette = cv2.kmeans(
+            pixels,
+            n_colors,
+            None,
+            criteria,
+            ConfigLoader.getVariable("color_detection", "attempts"),
+            flags,
+        )
+            color = Color(rgb=(palette[0][2],palette[0][1],palette[0][0]))
+
+            if contrast >= 0.99 and color.name == "BLACK":
                 highThresh, _ = cv2.threshold(gray, 0, 255, cv2.THRESH_TOZERO + cv2.THRESH_OTSU)
                 lowThresh = 0.85*highThresh
+            elif contrast >= 0.99 and color.name == "WHITE":
+                highThresh, _ = cv2.threshold(gray, 0, 255, cv2.THRESH_TOZERO + cv2.THRESH_OTSU)
+                lowThresh = -0.85*highThresh
             else:
                 highThresh, _ = cv2.threshold(gray, 80, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_TOZERO)
                 lowThresh = 0*highThresh
 
-            
             edges = cv2.Canny(gray, lowThresh, highThresh)
             edges = cv2.dilate(edges, None)
             edges = cv2.erode(edges, None)
-
+ 
             contour_info = []
             contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
             for c in contours:
